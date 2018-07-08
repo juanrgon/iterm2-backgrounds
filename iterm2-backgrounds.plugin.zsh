dfbf0ee66932870c6276ff50a6a3905165ab8e61
Juan Gonzalez <jrg2156@gmail.com>
2018-07-07 23:22:56 -0400

    Updates

--- iterm2-backgrounds.plugin.zsh
+++ iterm2-backgrounds.plugin.zsh
@@ -1,10 +1,10 @@
 function tabc() {
     if [[ -z $DEFAULT_ITERM_PROFILE ]]; then
-        default_profile=$DEFAULT_ITERM_PROFILE
-    else
         default_profile='Default'
+    else
+        default_profile=$DEFAULT_ITERM_PROFILE
     fi
-    NAME=$1; if [ -z "$NAME" ]; then NAME=$DEFAULT_ITERM_PROFILE; fi
+    NAME=$1; if [ -z "$NAME" ]; then NAME=$default_profile; fi
     # if you have trouble with this, change
     # "Default" to the name of your default theme
     echo -e "\033]50;SetProfile=$NAME\a"
@@ -12,15 +12,15 @@ function tabc() {
 
 function tab-reset() {
     if [[ -z $DEFAULT_ITERM_PROFILE ]]; then
-        default_profile=$DEFAULT_ITERM_PROFILE
-    else
         default_profile='Default'
+    else
+        default_profile=$DEFAULT_ITERM_PROFILE
     fi
     echo -e "\033]50;SetProfile=$default_profile\a"
 }
 
 function colordocker(){
-    if [[ "$*" =~ " -it "]]; then
+    if [[ "$*" =~ " -it " ]]; then
         trap "tab-reset" INT EXIT
         tabc Docker
     fi
@@ -28,7 +28,7 @@ function colordocker(){
 }
 
 function color-docker-compose(){
-    if [[ "$1" =~ "exec"]]; then
+    if [[ "$1" =~ "exec" ]]; then
         trap "tab-reset" INT EXIT
         tabc Docker
     fi

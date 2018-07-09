076d5356ebf614b9700cf0960450ba12be5a923d
Juan Gonzalez <jrg2156@gmail.com>
2018-07-09 14:59:44 -0400

    Cleanup

--- iterm2-backgrounds.plugin.zsh
+++ iterm2-backgrounds.plugin.zsh
@@ -1,44 +1,37 @@
 function tabc() {
-    if [[ -z $DEFAULT_ITERM_PROFILE ]]; then
-        default_profile='Default'
-    else
-        default_profile=$DEFAULT_ITERM_PROFILE
-    fi
-    NAME=$1; if [ -z "$NAME" ]; then NAME=$default_profile; fi
+    NAME=$1; if [ -z "$NAME" ]; then NAME=$ITERM_PROFILE; fi
     # if you have trouble with this, change
     # "Default" to the name of your default theme
     echo -e "\033]50;SetProfile=$NAME\a"
 }
 
 function tab-reset() {
-    if [[ -z $DEFAULT_ITERM_PROFILE ]]; then
-        default_profile='Default'
-    else
-        default_profile=$DEFAULT_ITERM_PROFILE
-    fi
-    echo -e "\033]50;SetProfile=$default_profile\a"
+    tabc $ITERM_PROFILE
 }
 
 function colordocker(){
     if [[ "$*" =~ " -it " ]]; then
-        trap "tab-reset" INT EXIT
+        trap "tab-reset" INT TERM
         tabc Docker
     fi
     docker $*
+    tab-reset
 }
 
 function color-docker-compose(){
     if [[ "$1" =~ "exec" ]]; then
-        trap "tab-reset" INT EXIT
+        trap "tab-reset" INT TERM
         tabc Docker
     fi
     docker-compose $*
+    tab-reset
 }
 
 function colorssh(){
-    trap "tab-reset" INT EXIT
+    trap "tab-reset" INT TERM
     tabc ssh
     ssh $*
+    tab-reset
 }
 
 alias ssh="colorssh"

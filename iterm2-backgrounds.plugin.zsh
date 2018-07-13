ec3083a9faedfc9f4e7c456c3e874e4dd1720aa1
Juan Gonzalez <jrg2156@gmail.com>
2018-07-13 12:43:28 -0400

    Update

--- iterm2-backgrounds.plugin.zsh
+++ iterm2-backgrounds.plugin.zsh
@@ -1,20 +1,24 @@
 autoload -Uz add-zsh-hook
-function tabc() {
-    PROFILE="${1:-$ITERM_PROFILE}"  # Name of the profile we're switching to
+function flash-iterm-profile() {
+    # Temporarily changes iTerm profile until the next command line prompt
+    #
+    # Args:
+    # $1 Name of the profile we're switching to; defaults to the original profile loaded
+    PROFILE=${1:-$ITERM_PROFILE}
 
     if [[ "$PROFILE" == $ITERM_PROFILE ]]; then
         # Add a hook to switch back to the default profile on the next prompt
-        add-zsh-hook -d precmd tabc
+        add-zsh-hook -d precmd load-terminal-profile
     else
         # Remove hook to switch back to the default profile on the next prompt
-        add-zsh-hook precmd tabc
+        add-zsh-hook precmd flash-iterm-profile
     fi
     echo -e "\033]50;SetProfile=$PROFILE\a"
 }
 
 function colordocker(){
     if [[ "$*" =~ " -it " || ("$*" =~ " -i "  && "$*" =~ " -t ")]]; then
-        tabc Docker
+        fip Docker
     fi
 
     disable -a docker
@@ -23,7 +27,7 @@ function colordocker(){
 
 function color-docker-compose(){
     if [[ "$1" == "exec" || "$1" == "run" ]]; then
-        tabc Docker
+        fip Docker
     fi
 
     disable -a docker-compose
@@ -31,12 +35,13 @@ function color-docker-compose(){
 }
 
 function colorssh(){
-    tabc ssh
+    fip ssh
 
     disable -a ssh
     ssh $*
 }
 
+alias fip='flash-iterm-profile'
 alias ssh="colorssh"
 alias docker="colordocker"
 alias docker-compose="color-docker-compose"
